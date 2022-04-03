71f87400c6d267aefa313973b98d88a643f041ff
Abhishek Kognole <aakognole@gmail.com>
2022-04-03 09:46:22 -0400

    setup pypi project

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
-from setuptools import setup, find_packages 
+from setuptools import setup, find_packages
 
-setup(name='LigPrepper', 
-      version='0.1.0', 
-      description='program to quickly prepare ligand 3d SDF from smiles', 
-      long_description="",
-      url='https://github.com/aakognole/ligprepper',
-      author='Abhishek Kognole',
-      author_email='aakognole@gmail.com',
-      license='MIT',
-      packages=find_packages(), 
-      include_package_data=False,
-      install_requires=['rdkit-pypi'], 
-      zip_safe=False)
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="LigPrepper",
+    version="0.1.0",
+    author="Abhishek Kognole",
+    author_email="aakognole@gmail.com",
+    description="Simple program to quickly prepare ligand 3d SDF from smiles",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aakognole/ligprepper",
+    project_urls={
+        "Bug Tracker": "https://github.com/aakognole/ligprepper/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    install_requires=['rdkit-pypi'],
+    python_requires=">=3.6",
+    zip_safe=False
+)
